824997f8aff02f986c9914b3052793ef1d6b682d
Isabelli Maciel Marques <44590917807@>
2024-01-25 10:53:19 -0300

    primeiro contato com react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1> Hello world! </h1>
+            <h2>My first React App</h2>
+        </header>
+      </div>
+    );
+}
+
+export default App;
